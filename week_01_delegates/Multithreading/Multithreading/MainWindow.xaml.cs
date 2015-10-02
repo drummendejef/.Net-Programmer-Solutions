fee2f884580b3daad5049838c39af2d7a1a0239e
Joren <jorenvandamme@hotmail.com>
2015-10-02 14:14:45 +0200

    Aanmaken github
    Week 1 was al gemaakt, Week 2 is begonnen.
    Aanmaken github en alles hier in smijten.

---
+++ week_01_delegates/Multithreading/Multithreading/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Multithreading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int i = 0;
+
+            while(true)
+            {
+                tellerTextBox.Text = i.ToString();
+                i++;
+                Console.WriteLine(i);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            BeginTellerEen teller = new BeginTellerEen()
+        }
+    }
+}
---
+++ week_01_delegates/events/events/LeegTekstVak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace events
+{
+    public delegate void EventTest(object sender, EventArgs args);//Delegate voor het event
+    public class LeegTekstVak : TextBox//Public maken, moet openbaar zijn voor overerving
+    {
+        public event EventTest TekstisNul;//eventdefinitie
+        public LeegTekstVak()
+        {
+            this.TextChanged += new TextChangedEventHandler(LeegTekstVak_TextChanged);
+        }
+
+        void LeegTekstVak_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.Text.Length == 0)
+                if (TekstisNul != null) //Minstens 1 client voor dit event nodig, anders null
+                    Console.WriteLine("Vak is leeg");
+                        //MessageBox.Show("Hoi");
+        }
+
+    }
+}
